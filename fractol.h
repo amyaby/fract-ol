895726a021a09b5931cad6783482d5860643addf
Imane Abasalah <113394974+amyaby@users.noreply.github.com>
2025-02-11 07:23:36 -0500

    not yet

---
+++ fractol.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iabasala <iabasala@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 16:30:44 by iabasala          #+#    #+#             */
+/*   Updated: 2025/02/11 01:43:46 by iabasala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+#define fractol_H
+
+#include <stdio.h>
+#include <mlx.h>
+#include <stdlib.h> // for exit()
+
+
+
+int ft_strcmp(char *s1,char *s2);
+
+#endif
--- main.c
+++ main.c
@@ -1,27 +1,51 @@
+#include "fractol.h"
 
-#include"minilibx-linux/mlx_int.h"
-#include "minilibx-linux/mlx.h"
+
+int handle_key(int keycode, void *param)
+{
+    (void)param;
+    if (keycode == 65307) // Escape key
+        exit(0);
+    return (0);
+}
+int close_window(void *param)
+{
+    (void)param;
+    exit(0);
+    return (0);
+}
 
 int main()
 {
     void *mlx;
     void *win;
 
-    // Initialize MLX
     mlx = mlx_init();
-    if (!mlx)
-        return (1);
-
-    // Create a 500x500 window
-    win = mlx_new_window(mlx, 500, 500, "My MLX Window");
-    if (!win)
-        return (1);
-
-    // Loop to keep the window open
+    win = mlx_new_window(mlx, 800, 600, "playing");
+    mlx_hook(win,17,0,close_window,NULL);
+    mlx_key_hook(win, handle_key, NULL);
+   //  mlx_pixel_put(mlx, win, 400, 300, 0xFF0000);
+   //       mlx_pixel_put(mlx, win, 399, 299, 0xFF0000);
+   //  mlx_pixel_put(mlx, win, 398, 298, 0xFF0000);
+//   int x = 200;
+//     while (x <= 600)  // Draw horizontal line
+//     {
+//         mlx_pixel_put(mlx, win, x, 100, 0xFF0000); // Red color
+//         x++;
+//     }
+int y = 250;
+    while (y < 325)  // Square height
+    {
+      int  x = 350;
+        while (x < 450)  // Square width
+        {
+            mlx_pixel_put(mlx, win, x, y, 0x0000FF); // Blue color
+            x++;
+        }
+        y++;
+    }
     mlx_loop(mlx);
-
     return (0);
 }
 
-//cc main.c -Lminilibx-linux -lmlx -lX11 -lXext
 
