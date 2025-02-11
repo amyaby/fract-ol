18759376d334d747aa901d5557f60aff98e4dd41
amyaby <ayaby@m.vm>
2025-02-11 07:03:21 -0500

    fractals

---
+++ main.c
@@ -0,0 +1,27 @@
+
+#include"minilibx-linux/mlx_int.h"
+#include "minilibx-linux/mlx.h"
+
+int main()
+{
+    void *mlx;
+    void *win;
+
+    // Initialize MLX
+    mlx = mlx_init();
+    if (!mlx)
+        return (1);
+
+    // Create a 500x500 window
+    win = mlx_new_window(mlx, 500, 500, "My MLX Window");
+    if (!win)
+        return (1);
+
+    // Loop to keep the window open
+    mlx_loop(mlx);
+
+    return (0);
+}
+
+//cc main.c -Lminilibx-linux -lmlx -lX11 -lXext
+
